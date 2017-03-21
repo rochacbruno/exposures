b4369fa137a81cfb4a579145e2e18819720da9a7
Steven Cox <scox@renci.org>
2017-03-21 07:37:11 -0400

    modular validation

--- app/app.py
+++ app/app.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+"""NCATS Translator Environmental Exposures API.
+
+This application provides an smartAPI compliant endpoint for retrieving
+environmental exposure data qualified by temporal and geographic constraints.
+
+It uses Flasgger which provides flexible support for combining Swagger and Flask.
+This includes support for validating post parameters according to a Swagger
+schema, versioned interfaces, CORS support, and other features.
+
+Todo:
+    * Swagger definitions are not yet working. Incorporate.
+    * You have to also use ``sphinx.ext.todo`` extension
+"""
 import os
 import traceback
 from flask import Flask, jsonify, request
@@ -7,13 +21,7 @@ from flasgger.utils import swag_from, validate, ValidationError
 
 app = Flask(__name__)
 
-
-# config your API specs
-# you can define multiple specs in the case your api has multiple versions
-# ommit configs to get the default (all views exposed in /spec url)
-# rule_filter is a callable that receives "Rule" object and
-#   returns a boolean to filter in only desired views
-
+""" Define a spec per API version. """
 app.config['SWAGGER'] = {
     "swagger_version": "2.0",
     "title": "Exposures",
@@ -40,22 +48,40 @@ app.config['SWAGGER'] = {
 
 swagger = Swagger(app)
 
+class Validation(object):
+    def __init__(self, message="", status=200):
+        self.message = message
+        self.status = status
+
+def validate_request (request, schema_id, swagger_spec):
+    """ Validate a swagger request against a specified schema object within the
+    given Swagger specification. 
+
+    Returns:
+        Validation: Returns a validation object [message,status]
+    
+    """
+    result = Validation ()
+    try: 
+        validate(request.json, schema_id, swagger_spec, root=__file__)
+    except ValidationError as e:
+        traceback.print_exc ()
+        result = Validation ("Validation Error: {0}".format (e), status=400)
+    return result
+
 @app.after_request
 def allow_origin(response):
-    response.headers['Access-Control-Allow-Origin'] = 'http://example.com'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Credentials'] = 'true'
     return response
 
 @app.route('/v1/getExposureValue', methods=['POST'], endpoint='should_be_v1_only_getExposureValue')
 @swag_from('swagger/getExposureValue.yml')
 def get_exposure_value():
-    print (request.json)
-    try: 
-        validate(request.json, 'exposureRequestSchema', 'swagger/getExposureValue.yml', root=__file__)
-    except ValidationError as e:
-        traceback.print_exc ()
-        print (str(e))
-        return "Validation Error: %s" % e, 400
+    """ Get exposure value. See swagger definition for further details. """
+    validation = validate_request (request, 'exposureRequestSchema', 'swagger/getExposureValue.yml')
+    if not validation.status == 200:
+        return validation.message, validation.status
     return jsonify([
         {
             'start-time' : 0,
@@ -67,13 +93,10 @@ def get_exposure_value():
 @app.route('/v1/getExposureScore', methods=['POST'], endpoint='should_be_v1_only_getExposureScore')
 @swag_from('swagger/getExposureScore.yml')
 def get_exposure_score():
-    print (request.json)
-    try: 
-        validate(request.json, 'exposureScoreRequestSchema', 'swagger/getExposureScore.yml', root=__file__)
-    except ValidationError as e:
-        traceback.print_exc ()
-        print (str(e))
-        return "Validation Error: %s" % e, 400
+    """ Get exposure score. See swagger spec for further details. """
+    validation = validate_request (request, 'exposureScoreRequestSchema', 'swagger/getExposureScore.yml')
+    if not validation.status == 200:
+        return validation.message, validation.status
     return jsonify([
         {
             'start-time' : 0,
